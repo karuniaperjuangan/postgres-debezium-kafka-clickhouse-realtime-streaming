fb887ed65aff7cab5da91add5535a5ad7a5daa11
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2025-01-28 16:34:03 +0700

    feat:update user aggregate query

--- clickhouse_query.sql
+++ clickhouse_query.sql
@@ -6,3 +6,10 @@ FROM kafka_transactions
 WHERE status = 'completed'
 GROUP BY user_id
 ORDER BY total_amount DESC;
+
+SELECT
+    user_id,
+    sumMerge(total_amount) AS total_amount,
+    countMerge(count_events) AS count_events
+FROM user_aggregates ua 
+GROUP BY user_id
